7f0b7a7e13226262a4a8fd9c5d6aa0bb73012ef4
moseyweb <moseyweb@gmail.com>
2023-12-24 05:54:22 +0000

    Updated serviceAccount to JSON format.

--- serviceAccount.js
+++ serviceAccount.js
@@ -16,14 +16,14 @@ const {
 } = process.env
 
 module.exports = {
-  type: TYPE,
-  project_id: SERVICE_PROJECT_ID,
-  private_key_id: PRIVATE_KEY_ID,
-  private_key: PRIVATE_KEY,
-  client_email: CLIENT_EMAIL,
-  client_id: CLIENT_ID,
-  auth_uri: AUTH_URI,
-  token_uri: TOKEN_URI,
-  auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: CLIENT_X509_CERT_URL
+  "type": TYPE,
+  "project_id": SERVICE_PROJECT_ID,
+  "private_key_id": PRIVATE_KEY_ID,
+  "private_key": PRIVATE_KEY,
+  "client_email": CLIENT_EMAIL,
+  "client_id": CLIENT_ID,
+  "auth_uri": AUTH_URI,
+  "token_uri": TOKEN_URI,
+  "auth_provider_x509_cert_url": AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": CLIENT_X509_CERT_URL
 }
